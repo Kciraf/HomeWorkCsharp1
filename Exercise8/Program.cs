dbf1765171ae654285fab7356bb2521869715666
Kciraf <valsinat44a@mail.ru>
2023-02-18 16:57:26 +0600

    Exercise 8

--- Exercise8/Program.cs
+++ Exercise8/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+
+if(num >=2)
+{
+    while (index <= num)
+    {
+        System.Console.Write($"{index} ");
+        index+=2;
+    }
+}
+else
+{
+    System.Console.WriteLine("Нет положительных четных чисел до указанного числа");
+}
